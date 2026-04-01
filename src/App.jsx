5e3be75fc94c30afabc5d6f8639c06a6b71f0f0c
muhammadsodiq25 <muhammadsodiq25032013@gmail.com>
2026-04-01 11:21:48 +0500

    sidebar,dashbaord

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
+import Akbarshox from "./pages/akbarshox/Akbarshox";
+import Abdulbosit from "./pages/abdulbosit/Abdulbosit";
+import Abdulaziz from "./pages/abdulaziz/Abdulaziz";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="flex w-full justify-between items-start">
+      <Sidebar />
+      <div className="flex flex-col gap-5 w-[90%]">
+        <Header />
+        <div className="w-[90%] flex flex-col gap-6">
+          <Routes>
+            <Route path="/" element={<Akbarshox />} />
+            <Route path="/abdulbosit" element={<Abdulbosit />} />
+            <Route path="/abdulaziz" element={<Abdulaziz />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { CiSearch } from "react-icons/ci";
+import { header_user } from "../../data/Muhammadsodiq";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="flex w-full items-center py-6 px-7 justify-between">
+      <div className="flex items-center gap-5">
+        <CiSearch className="text-2xl font-extrabold text-[#7E84A3]" />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="cursor-pointer font-bold text-wrap outline-0"
+        />
+      </div>
+      <div className="flex items-center gap-5">
+        <img
+          className="w-10 rounded-[100px] bg-[#D8D8D8]"
+          src={header_user.img}
+          alt=""
+        />
+        <h1 className="font-semibold text-lg text-[#5A607F]">
+          {header_user.text}
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
